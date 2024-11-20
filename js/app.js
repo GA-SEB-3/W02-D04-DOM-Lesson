a7cb682cd3862438179c203eadaa57fd7d495968
omarakamal <omaraakamal@gmail.com>
2024-11-20 18:49:02 +0300

    updated code

---
+++ js/app.js
@@ -0,0 +1,20 @@
+
+// querySelector(): fetches an element for me
+
+
+const mainTitleElement = document.querySelector("#main-title")
+
+console.log(mainTitleElement)
+
+
+const pElement = document.querySelector(".cool")
+
+// .textContent: changes the text in html tag
+pElement.textContent = "New value given in JS file"
+
+
+
+// 
+let myImageElement = document.querySelector("img")
+
+myImageElement.src = "https://cdn.pixabay.com/photo/2024/02/28/07/42/european-shorthair-8601492_640.jpg"
